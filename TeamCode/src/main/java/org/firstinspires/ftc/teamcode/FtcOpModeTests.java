37002da40e03ef71fd35978698a94c464c334759
Shrey Karmacharya <shreykarmacharya@gmail.com>
2023-11-12 10:09:11 -0800

    Updating local repo to sync with global repo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FtcOpModeTests.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FtcOpModeTests.java
@@ -1,2 +1,39 @@
-package org.firstinspires.ftc.teamcode;public class FtcOpModeTests {
+// All tests for the robot go in this file:
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.*;
+
+@TeleOp
+
+public class FtcOpModeTests extends LinearOpMode {
+    private Gyroscope imu;
+    private DcMotor motorTest;
+    private DigitalChannel digitalTouch;
+    private DistanceSensor sensorColorRange;
+    private Servo servoTest;
+
+
+    @Override
+    public void runOpMode() {
+        imu = hardwareMap.get(Gyroscope.class, "imu");
+        motorTest = hardwareMap.get(DcMotor.class, "motorTest");
+        digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
+        sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
+        servoTest = hardwareMap.get(Servo.class, "servoTest");
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+
+        }
+    }
 }
