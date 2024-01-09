fcc47feb6f5b85c6d8c8b23fde848c0d11e667ae
wafflesrgood <say.safalghimire711@outlook.com>
2024-01-09 11:16:21 -0800

    Added control of the hanger rotator and the roller placed on the box onto the gamepad controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeServoTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeServoTesting.java
@@ -2,32 +2,32 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
-
 public class IntakeServoTesting extends LinearOpMode {
 
-    private CRServo IntakeL, IntakeR;
+    private CRServo intakeL, intakeR;
 
     public void runOpMode() throws InterruptedException {
         telemetry.addData("Mode", "waiting");
         telemetry.update();
 
         //Initialization of Intake Wheels
-        IntakeR = hardwareMap.get(CRServo.class, "IntakeR");
-        IntakeL = hardwareMap.get(CRServo.class, "IntakeL");
+        intakeR = hardwareMap.crservo.get( "RightIntakeServo");
+
+        intakeL = hardwareMap.get(CRServo.class, "LeftIntakeServo");
         //
 
-
+       intakeR.resetDeviceConfigurationForOpMode();
         waitForStart();
+
+
         while (opModeIsActive()) {
             ////CODE///
-           IntakeR.setPower(1); //test change in direction
-           IntakeL.setPower(-1);
+           intakeR.setPower(1);
+           intakeL.setPower(-1);
 
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopTwoDrivers.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopTwoDrivers.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -9,47 +11,51 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp
 
 public class TeleopTwoDrivers extends LinearOpMode {
-    DcMotor ArmMotor;
-    private DcMotor frontLeft, frontRight, backLeft, backRight;
-    private Servo ClawServo;
-    private Servo Drone;
-    private Servo JointServo;
-    private DcMotor Intake;
-    private DcMotor Hanger;
+    private DcMotor FLWheel, BLWheel, HangerRotater, LinearActuator; //0, 1, 2, 3 CH
+    private CRServo LeftIntakeServo; //5, CH
+    private CRServo Roller; //TBD
+    private Servo Drone; //4, CH
+
+    //Expansion Hub
+    private DcMotor FRWheel, ViperSlide, BRWheel; //0, 1 Exp
+    private CRServo RightIntakeServo; //5 Exp
+    private Servo AutonomousServo, ViperSlideServo, WristServo; //4 Exp, 3 Exp, 2 Exp
+    //webcam is plugged into the control hub.
+
 
     public void runOpMode() throws InterruptedException {
         telemetry.addData("Mode", "waiting");
         telemetry.update();
-        //Initialization of Arm Motor
-        ArmMotor = hardwareMap.get(DcMotor.class, "ArmMotor");
-        ArmMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        Hanger = hardwareMap.get(DcMotor.class, "Hanging");
-        //
-        // Initialization of movement motors
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
-        //
-        //Initialization of Claw and wrist joint
-        ClawServo = hardwareMap.get(Servo.class, "ClawServo");
-        JointServo = hardwareMap.get(Servo.class, "JointServo");
-        //
-
-        //Initialization of Intake motor and Drone servo
-        Intake = hardwareMap.get(DcMotor.class, "Intake");
+        //Initialization of Control Hub things
+        //motors
+        FLWheel = hardwareMap.get(DcMotor.class, "FLWheel");
+        BLWheel = hardwareMap.get(DcMotor.class, "BLWheel");
+        HangerRotater = hardwareMap.get(DcMotor.class, "HangerRotater");
+        LinearActuator = hardwareMap.get(DcMotor.class, "LinearActuator");
+        //servos
+        LeftIntakeServo = hardwareMap.get(CRServo.class, "LeftIntakeServo");
         Drone = hardwareMap.get(Servo.class, "Drone");
-        //
+        Roller = hardwareMap.get(CRServo.class, "Roller");
+
+        //Initialization of Expansion Hub things
+        //motors
+        FRWheel = hardwareMap.get(DcMotor.class, "FRWheel");
+        ViperSlide = hardwareMap.get(DcMotor.class, "ViperSlide");
+        BRWheel = hardwareMap.get(DcMotor.class, "BRWheel");
+        HangerRotater = hardwareMap.get(DcMotor.class, "HangerRotater");
+        //servos
+        RightIntakeServo = hardwareMap.get(CRServo.class, "RightIntakeServo");
+        AutonomousServo = hardwareMap.get(Servo.class, "AutonomousServo");
+        ViperSlideServo = hardwareMap.get(Servo.class, "ViperSlideServo");
+        WristServo = hardwareMap.get(Servo.class, "WristServo");
 
         // Set motor directions if needed
-        frontLeft.setDirection(DcMotor.Direction.FORWARD);
-        frontRight.setDirection(DcMotor.Direction.REVERSE);
-        backLeft.setDirection(DcMotor.Direction.FORWARD);
-        backRight.setDirection(DcMotor.Direction.REVERSE);
+        //flip the right side wheels so that positive seems the same for both sides
+        BRWheel.setDirection(DcMotorSimple.Direction.REVERSE);
+        FRWheel.setDirection(DcMotorSimple.Direction.REVERSE);
 
         //multiplier and flippers
-        double multiplier = 0.3;
-        double flipper = 1;
+        double multiplier = 0.25;
 
 
         waitForStart();
@@ -57,62 +63,104 @@ public class TeleopTwoDrivers extends LinearOpMode {
             ////CODE FOR DRIVETRAIN ///
             double drive = -gamepad1.left_stick_x;
             double strafe = gamepad1.left_stick_y;
-            double rotate = flipper * gamepad1.right_stick_x;
+            double rotate = gamepad1.right_stick_x;
             double frontLeftPower = Range.clip(drive + strafe + rotate, -1.0, 1.0);
             double frontRightPower = Range.clip(drive - strafe - rotate, -1.0, 1.0);
             double backLeftPower = Range.clip(drive - strafe + rotate, -1.0, 1.0);
             double backRightPower = Range.clip(drive + strafe - rotate, -1.0, 1.0);
-            frontLeft.setPower(multiplier * frontLeftPower);
-            frontRight.setPower(multiplier * frontRightPower);
-            backLeft.setPower(multiplier * backLeftPower);
-            backRight.setPower(multiplier * backRightPower);
+            FLWheel.setPower(multiplier * frontLeftPower);
+            FRWheel.setPower(multiplier * frontRightPower);
+            BLWheel.setPower(multiplier * backLeftPower);
+            BRWheel.setPower(multiplier * backRightPower);
             //Updating telemetry
             telemetry.addData("Status", "Running");
             telemetry.addData("Front Left Power", frontLeftPower);
             telemetry.addData("Front Right Power", frontRightPower);
             telemetry.addData("Back Left Power", backLeftPower);
             telemetry.addData("Back Right Power", backRightPower);
-            telemetry.addData("Position", ArmMotor.getCurrentPosition());
+
             telemetry.update();
             telemetry.update();
             //
-
-
-            ///ARM MOVEMENT (GAMEPAD 2)
-            double ArmPower = gamepad2.right_stick_y;
-            ArmMotor.setPower(0.4 * ArmPower);
-            //Hanger Movement (GAMEPAD 2)
-            double HangPower = gamepad2.left_stick_y;
-            Hanger.setPower(HangPower * 0.8);
-
-
-            //Wrist Servo Control
-            if (gamepad2.a) { JointServo.setPosition(0.78);} //for grabbing?
-            if (gamepad2.b) { JointServo.setPosition(0.9);} //for lifting?
-            if (gamepad2.y) { JointServo.setPosition(1.3); }  //for placing?
-            //
-            //Claw Servo Control
-            if (gamepad2.right_bumper) { ClawServo.setPosition(0.75);} // open
-            if (gamepad2.left_bumper) { ClawServo.setPosition(0.5);} // close
-
 
             //Intake mill controls (GAMEPAD 1)
-            if (gamepad1.right_bumper) { Intake.setPower(0.5);}
-            if (gamepad1.left_bumper) { Intake.setPower(-0.5); }
-            if (gamepad1.a) { Intake.setPower(0);}
 
-            //Swap Speed! (Gamepad 1)
-            if (gamepad1.b) { multiplier = 0.8; }
-            if (gamepad1.y) { multiplier = 0.3; }
-
-            //Swap Direction! (Gamepad1)
-            if (gamepad1.x) { flipper = -1 * flipper ;}
-            //Drone Launch! (Gamepad2)
-            if (gamepad2.x)
+            if(gamepad1.right_bumper)
             {
-                Drone.setPosition(-1);
-                // Drone.setPosition(1);
-                // Drone.setPosition(0.5);
+                RightIntakeServo.setPower(1);
+                LeftIntakeServo.setPower(-1);
+            }
+            if(gamepad1.left_bumper)
+            {
+                RightIntakeServo.setPower(0);
+                LeftIntakeServo.setPower(0);
+            }
+            /////////////////////////////////////
+
+            //Servo Wrist Controls
+            if(gamepad1.a)
+            {
+                WristServo.setPosition(0); //og position
+            }
+            if (gamepad1.b)
+            {
+                WristServo.setPosition(0.5); //position up for possesing the pixels
+            }
+            if(gamepad1.x)
+            {
+                WristServo.setPosition(1); //position on the wat down for placing the pixels
+            }
+
+            ///////////////////////////////////
+
+            //Control of the extension of the viperslide
+            if(gamepad2.right_bumper)
+            {
+                ViperSlide.setPower(0.5);
+            }
+            if(gamepad2.left_bumper)
+            {
+                ViperSlide.setPower(-0.5);
+            }
+            if(gamepad2.dpad_right)
+            {
+                ViperSlide.setPower(0);
+            }
+
+            ///////////////////////////////////////
+
+            //Control of the Viperslide Servo
+            if(gamepad2.dpad_down)
+            {
+                ViperSlideServo.setPosition(0);//home position
+            }
+            if(gamepad2.dpad_left)
+            {
+                ViperSlideServo.setPosition(0.5); //start to tilt up
+            }
+            if(gamepad2.dpad_up)
+            {
+                ViperSlideServo.setPosition(1);//scoring position
+            }
+            /////////////////
+            //control of the linear actuator
+
+            LinearActuator.setPower(gamepad2.right_stick_y);
+            //Control of the Linear Actuator's Turner
+            HangerRotater.setPower(gamepad2.left_stick_y);
+            //direction of the wheel on the box
+            if(gamepad2.a)
+            {
+                Roller.setPower(1);
+            }
+            if(gamepad2.b)
+            {
+                Roller.setPower(-1);
+            }
+            //Drone Launch! (Gamepad2)
+            if (gamepad1.x)
+            {
+                Drone.setPosition(-0.5);
             }
         }
     }
