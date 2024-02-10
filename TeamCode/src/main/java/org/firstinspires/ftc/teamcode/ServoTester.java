f47be9279a020dfcff22b7c53579733018fa41e5
Shrey Karmacharya <shreykarmacharya@gmail.com>
2024-02-09 20:24:38 -0800

    WWWWWWW TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTester.java
@@ -2,8 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class ServoTester extends LinearOpMode {
@@ -11,16 +10,52 @@ public class ServoTester extends LinearOpMode {
     @Override
     public void runOpMode() {
         // Initialize hardware
-        CRServo ts1 = hardwareMap.crservo.get("TrayServo1");
-        CRServo ts2 = hardwareMap.crservo.get("TrayServo1");
-
-        ts2.setDirection(DcMotorSimple.Direction.REVERSE);
+        Servo auto = hardwareMap.servo.get("auto");
 
         waitForStart();
         while (opModeIsActive()) {
+            if (gamepad1.a) {
+                auto.setPosition(0.1);
+            }
+            if (gamepad1.b) {
+                auto.setPosition(0.125);
+            }
+            if (gamepad1.x) {
+                auto.setPosition(0.25);
+            }
+            if (gamepad1.y) {
+                auto.setPosition(0.375);
+            }
+
+            if (gamepad2.a) {
+                auto.setPosition(0.5); // up
+            }
+            if (gamepad2.b) {
+                auto.setPosition(0.625);
+            }
+            if (gamepad2.x) {
+                auto.setPosition(0.75);
+            }
+            if (gamepad2.y) {
+                auto.setPosition(0.825);
+            }
+
+            if (gamepad1.back) {
+                auto.setPosition(0);
+            }
+            if (gamepad1.start) {
+                auto.setPosition(0.05);
+            }
+
+            if (gamepad2.back) {
+                auto.setPosition(0.95);
+            }
+            if (gamepad2.start) {
+                auto.setPosition(1);
+            }
 
             telemetry.update();
         }
-        ts1.setPower(0);
+        auto.setPosition(0);
     }
 }
